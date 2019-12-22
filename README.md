9bd34b6235d8c7727688847d32a9f143bd2a5f5c
pengliangren <31616557+pengliangren@users.noreply.github.com>
2019-12-22 15:00:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gulp_demo
+ gulp study demo
---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+function defaultTask(cb) {
+    // place code for your default task here
+    console.log('gulp task');
+    cb();
+}
+
+exports.default = defaultTask;
